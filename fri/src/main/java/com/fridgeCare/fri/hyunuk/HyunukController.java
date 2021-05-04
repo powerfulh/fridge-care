0ae6e4c57f1ce4afc8f1f65880d134a0c2a66439
t2103028 <t2103028@partner.coway.co.kr>
2021-05-04 14:56:18 +0900

    클라에서 보낸 제이슨 받아보기

--- fri/src/main/java/com/fridgeCare/fri/hyunuk/HyunukController.java
+++ fri/src/main/java/com/fridgeCare/fri/hyunuk/HyunukController.java
@@ -1,15 +1,9 @@
 package com.fridgeCare.fri.hyunuk;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import com.fridgeCare.fri.hyunuk.vo.GetVO;
-import com.fridgeCare.fri.hyunuk.vo.SearchVO;
 
 
 @Controller
@@ -17,52 +11,18 @@ import com.fridgeCare.fri.hyunuk.vo.SearchVO;
 public class HyunukController {
 	@Autowired
 	SearchDao sDao;
-	// 페이지 요청
 	@RequestMapping("/search.fri")
 	@ResponseBody
 	@CrossOrigin(origins = "http://localhost:8080")
-	public String getList() {
-		System.out.println("call from vue");
+	public String getList(String data1) {
+		System.out.println(data1);
+		
 		return "{\"result\":\"OK\"}";
 	}
-	// 선택된 값 요청
 	@RequestMapping(value="/selected.fri", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> SelectedIngred(String[] Situat, String[] Category, String SelIng) {
-		String situ = "";
-		String CAT = "";
-		if(Situat == null || Situat.length == 0) {
-			situ = "'SELF','DRINK','SIDE','FULL'";
-		}else {
-			for(int i = 0; i < Situat.length; i++) {
-				situ += "'"+(String)Situat[i] + "',";
-			}
-			situ = situ.substring(0,situ.length()-1);
-		}
-		if(Category == null || Category.length == 0) {
-			CAT = "'KR','CH','JP','WE','FU'";
-		}else {
-			for(int i = 0; i < Category.length; i++) {
-				CAT += "'"+(String)Category[i] + "',";
-			}
-			CAT = CAT.substring(0,CAT.length()-1);
-		}
-		if(SelIng == "") {
-			SelIng = "|";
-		}
-		GetVO gVO = new GetVO();
-		gVO.setCat(CAT);
-		gVO.setSitu(situ);
-		gVO.setSeling(SelIng);
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("CAT", CAT);
-		map.put("situ", situ);
-		map.put("SelIng", SelIng);
-		List<SearchVO> sLIST = sDao.getRecipelist(map);
-		System.out.println(sLIST.size());
-		Map<String,Object> map2 = new HashMap<String, Object>();
-		map2.put("sLIST",sLIST);
-		return map2;
+	public String SelectedIngred() {
+		return "{\"result\":\"OK\"}";
 	}
 	
 }
--- fri/src/main/java/com/fridgeCare/fri/hyunuk/SearchDao.java
+++ fri/src/main/java/com/fridgeCare/fri/hyunuk/SearchDao.java
@@ -1,46 +1,14 @@
 package com.fridgeCare.fri.hyunuk;
 
-import java.util.List;
-import java.util.Map;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fridgeCare.fri.hyunuk.vo.GetVO;
-import com.fridgeCare.fri.hyunuk.vo.IngredVO;
-import com.fridgeCare.fri.hyunuk.vo.SearchVO;
+import com.fridgeCare.fri.hh.vo.InputVO;
 
 public class SearchDao {
 	@Autowired
 	SqlSessionTemplate sqlSession;
-	
-	// 게시물 조회
-	public List<SearchVO> getRecipelist(Map<String, String> map){
-		return sqlSession.selectList("sSQL.getRecipelist", map);
+	public int logincheck(InputVO ivo) {
+		return sqlSession.selectOne("hsql.logincheck", ivo); // 아직 dao는 손 안댔다 이제 대야지
 	}
-
-	// 재료 채워주는 함수
-	public int getData(GetVO gVO) {
-		return sqlSession.selectOne("sSQL.getIngredient");
-	}
-	
-	// 재료 전부 조회 
-	public List<IngredVO> getIngredient(IngredVO inVO) {
-		return sqlSession.selectList("sSQL.getIngredient", inVO);
-	}
-	
-	// 타이틀 검색
-	public List<SearchVO> titleSearch(Map<String, Object> map) {
-		return sqlSession.selectList("sSQL.TitleSearch", map);
-	}
-	
-	// 조회수 증가
-	public int increseCnt(int bno) {
-		return sqlSession.update("sSQL.increseCnt");
-	}
-	
-	public int searchCNT(String search) {
-		return sqlSession.selectOne("sSQL.searchCNT");
-	}
-	
 }
